ca8c25bc79c158c6025c5d5722e1b923cf164989
maragung <50156713+maragung@users.noreply.github.com>
2023-10-15 14:45:02 +0700

    Update swap_area_manager_ubuntu.sh

--- swap_area_manager_ubuntu.sh
+++ swap_area_manager_ubuntu.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 
-create_swap() {
-    read -p "Enter the swap size in gigabytes (e.g., 2 for 2GB): " swap_size_gb
-    swap_size_mb=$((swap_size_gb * 1024))
-
-    echo "Creating a swap file of size $swap_size_gb GB..."
-    sudo fallocate -l ${swap_size_mb}M /swapfile
-
-    echo "Setting permissions on the swap file..."
-    sudo chmod 600 /swapfile
-
-    echo "Setting up swap area..."
-    sudo mkswap /swapfile
-
-    echo "Enabling the swap file..."
-    sudo swapon /swapfile
-
-    echo "Swap file has been created and enabled."
+check_existing_swap() {
+    echo "Checking existing swap area..."
     sudo swapon --show
-
-    # Add an entry for the swapfile in /etc/fstab
-    echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab
-
-    echo -e '\nSwap file added successfully.\n'
 }
 
 resize_swap() {
     if [ -e /swapfile ]; then
-        echo "Disabling the swap file before resizing..."
-        sudo swapoff /swapfile
-
         read -p "Enter the new swap size in gigabytes (e.g., 4 for 4GB): " new_swap_size_gb
         new_swap_size_mb=$((new_swap_size_gb * 1024))
 
@@ -46,6 +23,42 @@ resize_swap() {
         sudo swapon --show
     else
         echo "No swap file found. Please create a swap file first."
+    fi
+}
+
+create_swap() {
+    if [ -e /swapfile ]; then
+        echo -e "Swap file already exists.\n"
+
+        read -p "Do you want to resize the existing swap file? (y/n): " resize_option
+        if [ "$resize_option" == "y" ] || [ "$resize_option" == "Y" ]; then
+            resize_swap
+        else
+            echo "Aborting. No changes made to the existing swap file."
+        fi
+    else
+        read -p "Enter the swap size in gigabytes (e.g., 2 for 2GB): " swap_size_gb
+        swap_size_mb=$((swap_size_gb * 1024))
+
+        echo "Creating a swap file of size $swap_size_gb GB..."
+        sudo fallocate -l ${swap_size_mb}M /swapfile
+
+        echo "Setting permissions on the swap file..."
+        sudo chmod 600 /swapfile
+
+        echo "Setting up swap area..."
+        sudo mkswap /swapfile
+
+        echo "Enabling the swap file..."
+        sudo swapon /swapfile
+
+        echo "Swap file has been created and enabled."
+        sudo swapon --show
+
+        # Add an entry for the swapfile in /etc/fstab
+        echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab
+
+        echo -e '\nSwap file added successfully.\n'
     fi
 }
 
@@ -66,19 +79,21 @@ enable_swap() {
 
 # Main script
 echo "Select an option:"
-echo "1. Create Swap Area"
-echo "2. Resize Swap Area"
-echo "3. Disable Swap Area"
-echo "4. Enable Swap Area"
-echo "5. Exit"
+echo "1. Check Existing Swap Area"
+echo "2. Create Swap Area"
+echo "3. Resize Swap Area"
+echo "4. Disable Swap Area"
+echo "5. Enable Swap Area"
+echo "6. Exit"
 
 read -p "Enter your choice: " choice
 
 case $choice in
-    1) create_swap ;;
-    2) resize_swap ;;
-    3) disable_swap ;;
-    4) enable_swap ;;
-    5) echo "Exiting script. Goodbye!"; exit 0 ;;
+    1) check_existing_swap ;;
+    2) create_swap ;;
+    3) resize_swap ;;
+    4) disable_swap ;;
+    5) enable_swap ;;
+    6) echo "Exiting script. Goodbye!"; exit 0 ;;
     *) echo "Invalid choice. Exiting script."; exit 1 ;;
 esac
