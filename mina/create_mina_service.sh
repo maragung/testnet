b2953ab00cda5ac4c550842a0aa039d648da0734
maragung <50156713+maragung@users.noreply.github.com>
2023-10-16 20:41:21 +0700

    Create create_mina_service.sh

---
+++ mina/create_mina_service.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo -e "\033[32m"
+cat << "EOF"
+                  _                         _            
+  __   __    ____ (_)__  ____   ____  _   _ (_)__    ____ 
+ (__)_(__)  (____)(____)(____) (____)(_) (_)(____)  (____)
+(_) (_) (_)( )_( )(_)  ( )_( )( )_(_)(_)_(_)(_) (_)( )_(_)
+(_) (_) (_) (__)_)(_)   (__)_) (____) (___) (_) (_) (____)
+                              (_)_(_)              (_)_(_)
+                               (___)                (___)
+EOF
+echo -e "\033[0m"
+
+echo "Creating Mina service..."
+echo "Please wait..."
+
+sudo tee /etc/systemd/system/mina.service > /dev/null << EOF
+[Unit]
+Description=Mina Daemon Service
+After=network.target
+StartLimitIntervalSec=60
+StartLimitBurst=3
+
+[Service]
+Environment="PEERS_LIST_URL=https://storage.googleapis.com/seed-lists/testworld-2-0_seeds.txt"
+Environment="LOG_LEVEL=Info"
+Environment="FILE_LOG_LEVEL=Debug"
+EnvironmentFile=%h/.mina-env
+Type=simple
+Restart=always
+RestartSec=30
+ExecStart=/usr/local/bin/mina daemon --log-json --log-snark-work-gossip true --internal-tracing --insecure-rest-server --log-level Debug --file-log-level Debug --config-directory /root/.mina-config/ --external-ip $(wget -qO- ipinfo.io/ip) \
+--itn-keys  f1F38+W3zLcc45fGZcAf9gsZ7o9Rh3ckqZQw6yOJiS4=,6GmWmMYv5oPwQd2xr6YArmU1YXYCAxQAxKH7aYnBdrk=,ZJDkF9EZlhcAU1jyvP3m9GbkhfYa0yPV+UdAqSamr1Q=,NW2Vis7S5G1B9g2l9cKh3shy9qkI1lvhid38763vZDU=,Cg/8l+JleVH8yNwXkoLawbfLHD93Do4KbttyBS7m9hQ= \
+--itn-graphql-port 3089 --uptime-submitter-key  /root/keys/my-wallet --uptime-url https://block-producers-uptime-itn.minaprotocol.tools/v1/submit --metrics-port 10001 \
+--enable-peer-exchange  true --libp2p-keypair /root/keys/keys --log-precomputed-blocks true --max-connections 200 \
+--peer-list-url  https://storage.googleapis.com/seed-lists/testworld-2-0_seeds.txt --generate-genesis-proof  true \
+--block-producer-key /root/keys/my-wallet --node-status-url https://nodestats-itn.minaprotocol.tools/submit/stats  --node-error-url https://nodestats-itn.minaprotocol.tools/submit/stats \
+--file-log-rotations 500
+ExecStop=/usr/local/bin/mina client stop-daemon
+
+[Install]
+WantedBy=default.target
+EOF
+
+echo "Mina service has been created successfully."
+
+echo "To start the Mina service, use: sudo systemctl start mina"
+echo "To stop the Mina service, use: sudo systemctl stop mina"
+echo "To restart the Mina service, use: sudo systemctl restart mina"
+echo "To check the status of the Mina service, use: sudo systemctl status mina"
