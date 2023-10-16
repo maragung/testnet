c8796854d76f9f7b0ff6e6015594e7907be0a4b8
maragung <50156713+maragung@users.noreply.github.com>
2023-10-16 12:54:26 +0700

    Update install_mina_manager_testworld_2.0.sh

--- install_mina_manager_testworld_2.0.sh
+++ install_mina_manager_testworld_2.0.sh
@@ -8,7 +8,7 @@ source ~/.profile
     
 install_dependencies() {
     echo "Installing dependencies..."
-    apt-get install curl, wget, jq
+    apt-get install curl wget jq
     wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
     sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
 
