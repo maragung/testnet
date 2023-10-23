e7e4eae1546334d6ac463717381b2216a3f72af4
maragung <50156713+maragung@users.noreply.github.com>
2023-10-23 22:37:18 +0700

    Update update_testworld_2.0.0rampup6-4061884_ubuntu.sh

--- mina/update_testworld_2.0.0rampup6-4061884_ubuntu.sh
+++ mina/update_testworld_2.0.0rampup6-4061884_ubuntu.sh
@@ -14,4 +14,10 @@ echo -e "\033[0m"
 
 echo "Update [Berkeley Testnet Release 2.0.0rampup6 (ITN RC2)]"
 sudo apt-get update
-
+sudo systemctl stop mina
+sudo apt-get install -y mina-berkeley=2.0.0rampup6-4061884
+echo "Mina node has been updated successfully."
+mina version
+sudo systemctl start mina
+echo "To restart the Mina service, use: sudo systemctl restart mina"
+echo "To check the status of the Mina service, use: sudo systemctl status mina"
