3325774a8250b4100ffd2d84485b5bf011ba71c7
maragung <50156713+maragung@users.noreply.github.com>
2023-10-25 08:52:20 +0700

    Update update_namada_v0.23.1.sh

--- namada/update_namada_v0.23.1.sh
+++ namada/update_namada_v0.23.1.sh
@@ -4,8 +4,8 @@ wget https://github.com/anoma/namada/releases/download/v0.23.1/namada-v0.23.1-Li
 tar xvzf namada-v0.23.1-Linux-x86_64.tar.gz
 rm namada-v0.23.1-Linux-x86_64.tar.gz
 cd namada-v0.23.1-Linux-x86_64
-sudo systemctl stop namadad
 echo "Stopping service..."
+sudo systemctl stop namadad
 sleep 10
 sudo cp ./namada* /usr/local/bin/
 sudo systemctl start namadad
