6143558e5a03181e7272e3fef03535a3f09fe5ae
maragung <50156713+maragung@users.noreply.github.com>
2023-10-25 14:30:19 +0700

    Create create_namada_service.sh

---
+++ namada/create_namada_service.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Make sure you are in the correct directory
+cd /etc/systemd/system/
+
+# Check if the 'namadad.service' service file already exists
+if [ -f namadad.service ]; then
+  echo "Deleting the existing 'namadad.service' service."
+  systemctl stop namadad
+  systemctl disable namadad
+  rm namadad.service
+fi
+
+# Get the execution location of 'namada' from the result of 'which namada'
+NAMADA_EXEC=$(which namada)
+
+# Add the contents of the 'namadad.service' service file
+cat <<EOL > namadad.service
+[Unit]
+Description=namada
+After=network-online.target
+
+[Service]
+User=$USER
+WorkingDirectory=$HOME/.local/share/namada
+Environment=CMT_LOG_LEVEL=p2p:none,pex:error
+Environment=NAMADA_CMT_STDOUT=true
+ExecStart=$NAMADA_EXEC node ledger run
+StandardOutput=syslog
+StandardError=syslog
+Restart=always
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Reload systemd
+systemctl daemon-reload
+
+# Enable and start the service
+cd
+systemctl enable namadad
+systemctl start namadad
+
+echo "The 'namadad' service has been recreated and re-enabled."
+echo "To restart service: sudo systemctl start namadad"
+echo "To checking block status: curl http://127.0.0.1:26657/status | jq"
+echo "To view node logs: sudo journalctl -u namadad -f -o cat"
