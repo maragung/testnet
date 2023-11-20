33d666b3d995715ef444bc5f5a79af4f41df0bcf
maragung <50156713+maragung@users.noreply.github.com>
2023-11-20 12:36:15 +0700

    Update install_ubuntu.sh

--- arkeo/install_ubuntu.sh
+++ arkeo/install_ubuntu.sh
@@ -1,13 +1,25 @@
 sudo apt-get update
 sudo apt-get install -y build-essential curl wget jq make gcc chrony git
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 sudo su -c "echo 'fs.file-max = 65536' >> /etc/sysctl.conf"
 sudo sysctl -p
 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 sudo rm -rf /usr/local/.go
-wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz
+wget -O https://go.dev/dl/go1.19.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.19.2.linux-amd64.tar.gz
 sudo cp /usr/local/go /usr/local/.go -r
 sudo rm -rf /usr/local/go
+rm go1.19.2.linux-amd64.tar.gz
 
 cat <<'EOF' >>$HOME/.profile
 export GOROOT=/usr/local/.go
