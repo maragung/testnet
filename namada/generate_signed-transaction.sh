674ce8e901270deb25eec7c0530d5373cb9120a6
maragung <50156713+maragung@users.noreply.github.com>
2023-12-13 10:46:56 +0700

    Create generate_signed-transaction.sh
    for namada testnet-15

---
+++ namada/generate_signed-transaction.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Input prompts for the user
+read -p "Enter your ALIAS (your_moniker): " ALIAS
+read -p "Enter your EMAIL (your_email): " EMAIL
+read -p "Enter your IP (ip): " IP_SUFFIX
+read -p "Enter your VALIDATOR_ALIAS (Validator Name): " VALIDATOR_ALIAS
+
+# Confirmation of entered data
+echo "Entered Data:"
+echo "ALIAS: $ALIAS"
+echo "EMAIL: $EMAIL"
+echo "IP: $IP_SUFFIX:26656"
+echo "VALIDATOR_ALIAS: $VALIDATOR_ALIAS"
+
+read -p "Is the entered data correct? (y/n): " CONFIRMATION
+
+# Check user confirmation
+if [ "$CONFIRMATION" != "y" ]; then
+    echo "Please re-run the script and enter the correct data."
+    exit 1
+fi
+
+# Combining IP with port
+IP="$IP_SUFFIX:26656"
+
+# Path for the transactions.toml file
+TX_FILE_PATH="$HOME/.local/share/namada/pre-genesis/transactions.toml"
+
+# Execute the command once to get the established account address
+ESTABLISHED_ACCOUNT=$(namadac utils init-genesis-established-account --path $TX_FILE_PATH --aliases $ALIAS)
+ESTABLISHED_ACCOUNT_ADDRESS=$(echo "$ESTABLISHED_ACCOUNT" | grep -oP 'Account address: \K(.*)')
+
+# Generate pre-genesis key after confirmation
+namadaw --pre-genesis key gen --alias $ALIAS
+
+# Initialize validator
+namadac utils init-genesis-validator --address $ESTABLISHED_ACCOUNT_ADDRESS --alias $VALIDATOR_ALIAS --net-address $IP --commission-rate 0.05 --max-commission-rate-change 0.01 --self-bond-amount $SELF_BOND_AMOUNT --email $EMAIL --path $TX_FILE_PATH
+
+# Sign transactions
+namadac utils sign-genesis-txs \
+    --path $TX_FILE_PATH \
+    --output $HOME/.local/share/namada/pre-genesis/signed-transactions.toml \
+    --alias $VALIDATOR_ALIAS
+
+
