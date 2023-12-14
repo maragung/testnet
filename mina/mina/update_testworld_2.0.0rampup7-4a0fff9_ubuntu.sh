0378a514bc75aecad5fdb6aa50934d43821f5531
maragung <50156713+maragung@users.noreply.github.com>
2023-12-14 10:23:51 +0700

    Create update_testworld_2.0.0rampup7-4a0fff9_ubuntu.sh

---
+++ mina/mina/update_testworld_2.0.0rampup7-4a0fff9_ubuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo -e "\033[32m"
+cat << "EOF"
+                  _                         _            
+  __   __    ____ (_)__  ____   ____  _   _ (_)__    ____ 
+ (__)_(__)  (____)(____)(____) (____)(_) (_)(____)  (____)
+(_) (_) (_)( )_( )(_)  ( )_( )( )_(_)(_)_(_)(_) (_)( )_(_)
+(_) (_) (_) (__)_)(_)   (__)_) (____) (___) (_) (_) (____)
+                              (_)_(_)              (_)_(_)
+                               (___)                (___)
+EOF
+echo -e "\033[0m"
+
+echo "Update [Berkeley Testnet Release 2.0.0rampup7-4a0fff9 (ITN RC3)]"
+sudo apt-get update
+sudo systemctl stop mina
+sudo apt-get install -y mina-berkeley=2.0.0rampup7-4a0fff9
+wait
+echo "Mina node has been updated successfully."
+mina version
+wait
+sudo systemctl start mina
+wait
+echo "To restart the Mina service, use: sudo systemctl restart mina"
+echo "To check the status of the Mina service, use: sudo systemctl status mina"
+echo "Read node logs, use: sudo journalctl -u mina -n 1000 -f"
+echo "Read mina status, use: mina client status"
