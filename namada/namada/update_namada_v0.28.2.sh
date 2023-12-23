cf755c0ac4ca077c6853d245e8ae33973df0f2b1
maragung <50156713+maragung@users.noreply.github.com>
2023-12-23 21:38:53 +0700

    Create update_namada_v0.28.2.sh

---
+++ namada/namada/update_namada_v0.28.2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Downloading updated files......"
+wget -O namada-v0.28.2-Linux-x86_64.tar.gz https://github.com/anoma/namada/releases/download/v0.28.2/namada-v0.28.2-Linux-x86_64.tar.gz
+tar xvzf namada-v0.28.2-Linux-x86_64.tar.gz
+rm namada-v0.28.2-Linux-x86_64.tar.gz
+cd namada-v0.28.2-Linux-x86_64
+echo "Stopping service..."
+sudo systemctl stop namadad
+wait
+sudo cp ./namada* /usr/local/bin/
+wait
+sudo systemctl start namadad
+wait
+echo "Service started successfully."
+sudo journalctl -u namadad -f -o cat
