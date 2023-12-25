9e09282211eed1e2a485638148e258c1cdd8dcfa
maragung <50156713+maragung@users.noreply.github.com>
2023-12-25 15:10:01 +0700

    Update install_namada_telegram_bot.sh

--- namada/install_namada_telegram_bot.sh
+++ namada/install_namada_telegram_bot.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg make build-essential
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install nodejs -y
+
 # Get the directory where the script is located
 current_directory=$PWD
 
