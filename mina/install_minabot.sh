4cd6b0b2d9cf95c48c59a37c060a62afe21d0f12
maragung <50156713+maragung@users.noreply.github.com>
2023-12-25 15:13:12 +0700

    Update install_minabot.sh

--- mina/install_minabot.sh
+++ mina/install_minabot.sh
@@ -11,6 +11,17 @@ cat << "EOF"
 EOF
 echo -e "\033[0m"
 
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg make build-essential
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install nodejs -y
+
 echo "This is Mina Bot, used to monitor Mina Nodes in Testworld 2.0. Type /start in your bot to start"
 sleep 3
 git clone https://github.com/maragung/testworld_bot
