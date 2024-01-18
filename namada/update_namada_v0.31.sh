cc011c7d1cfe69d0ee2a85efe5951ba5fc814279
maragung <50156713+maragung@users.noreply.github.com>
2024-01-18 22:14:02 +0700

    Create update_namada_v0.31.sh

---
+++ namada/update_namada_v0.31.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Downloading updated files......"
+wget -O namada-v0.30.1-Linux-x86_64.tar.gz https://github.com/anoma/namada/releases/download/v0.30.1/namada-v0.30.1-Linux-x86_64.tar.gz
+tar xvzf namada-v0.30.1-Linux-x86_64.tar.gz
+rm namada-v0.30.1-Linux-x86_64.tar.gz
+cd namada-v0.30.1-Linux-x86_64
+echo "Stopping service..."
+sudo systemctl stop namadad
+wait
+sudo cp ./namada* /usr/local/bin/
+namada -V
+wait
+sudo systemctl start namadad
+wait
+echo "Service started successfully."
+sudo journalctl -u namadad -f -o cat
