699487b9b4084dcecdaf41624db675ac677b80b0
maragung <50156713+maragung@users.noreply.github.com>
2024-02-01 11:26:22 +0700

    Create install_namada_0.31.0_ubuntu_x64.sh

---
+++ namada/install_namada_0.31.0_ubuntu_x64.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Downloading files......"
+wget https://github.com/cometbft/cometbft/releases/download/v0.37.2/cometbft_0.37.2_linux_amd64.tar.gz
+tar xvzf cometbft_0.37.2_linux_amd64.tar.gz
+sudo cp ./cometbft /usr/local/bin/
+chmod +x /usr/local/bin/cometbft
+rm -rf cometbft_0.37.2_linux_amd64.tar.gz
+
+
+wget -O namada-v0.31.0-Linux-x86_64.tar.gz https://github.com/anoma/namada/releases/download/v0.31.0/namada-v0.31.0-Linux-x86_64.tar.gz
+tar xvzf namada-v0.31.0-Linux-x86_64.tar.gz
+rm namada-v0.31.0-Linux-x86_64.tar.gz
+cd namada-v0.31.0-Linux-x86_64
+wait
+sudo cp ./namada* /usr/local/bin/
+namada -V
