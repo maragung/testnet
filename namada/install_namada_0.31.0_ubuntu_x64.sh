21a5652ba3549ec06241c4b7e1f2c6a682f6d29d
maragung <50156713+maragung@users.noreply.github.com>
2024-02-02 04:34:42 +0700

    Update install_namada_0.31.0_ubuntu_x64.sh

--- namada/install_namada_0.31.0_ubuntu_x64.sh
+++ namada/install_namada_0.31.0_ubuntu_x64.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo "......................................."
-sudo systemctl stop namadad
-wait
-sudo systemctl disable namadad
-wait
-
 echo "Downloading files......"
 wget https://github.com/cometbft/cometbft/releases/download/v0.37.2/cometbft_0.37.2_linux_amd64.tar.gz
 tar xvzf cometbft_0.37.2_linux_amd64.tar.gz
@@ -25,31 +19,6 @@ export CHAIN_ID="shielded-expedition.b40d8e9055"
 namada client utils join-network --chain-id $CHAIN_ID
 wait
 
-sudo tee /etc/systemd/system/namadad.service > /dev/null <<EOF
-[Unit]
-Description=namada
-After=network-online.target
-[Service]
-User=$USER
-WorkingDirectory=$HOME/.local/share/namada
-Environment=CMT_LOG_LEVEL=p2p:none,pex:error
-Environment=NAMADA_CMT_STDOUT=true
-ExecStart=/usr/local/bin/namada node ledger run 
-StandardOutput=syslog
-StandardError=syslog
-Restart=always
-RestartSec=10
-LimitNOFILE=65535
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl daemon-reload
-wait
-sudo systemctl enable namadad
-wait
-sudo systemctl start namadad
-wait
 
 echo "Show node logs: sudo journalctl -u namadad -f -o cat"
 
