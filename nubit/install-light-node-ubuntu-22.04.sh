140e1eec9a625f18aca44cdcc5d3eb0e276bbb39
Maragung <50156713+maragung@users.noreply.github.com>
2024-06-24 22:14:57 +0700

    Update install-light-node-ubuntu-22.04.sh

--- nubit/install-light-node-ubuntu-22.04.sh
+++ nubit/install-light-node-ubuntu-22.04.sh
@@ -150,6 +150,7 @@ case $choice in
         ;;
 esac
 
+
 echo "Process complete. To start the service: sudo systemctl start nubitd"
 echo "To restart the service: sudo systemctl restart nubitd"
 echo "To stop the service: sudo systemctl stop nubitd"
